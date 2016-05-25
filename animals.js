48e2f1a1ffe9f889b04bf753503defaddca56661
Sergey Ivanov <sergey.ivanov@meta.ua>
2016-05-25 12:30:40 +0300

    add: OOP

--- animals.js
+++ animals.js
@@ -6,15 +6,16 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function Cat() {
-    this.name = "cat";
-    this.font = getRandomInt(18, 36) + "px serif";
-    this.text = "C";
-    this.x = getRandomInt(10, 450);
-    this.y = getRandomInt(10, 450);
+function Monster() {
+    this.name = "noname";
+    this.pixel = getRandomInt(18, 36);
+    this.font =  this.pixel + "px serif";
+    this.text = "+";
+    this.x = getRandomInt(0, canvas.width - this.pixel);
+    this.y = getRandomInt(this.pixel, canvas.width);
     this.vx = getRandomInt(0, 5);
     this.vy = getRandomInt(0, 2);
-    this.color = "blue";
+    this.color = "black";
     this.show = function() {
         ctx.font = this.font;
         ctx.fillStyle = this.color;
@@ -23,44 +24,39 @@ function Cat() {
     this.print = function() {
         console.log(this.name + ": x: "+this.x +" y: "+this.y);
     }
+    this.draw = function() {
+        this.show();
+        this.x += this.vx;
+        this.y += this.vy;
+        if ((this.y + this.vy) > canvas.height || this.y + this.vy < this.pixel) {
+            this.vy = -this.vy;
+        }
+        if ((this.x + this.vx) > (canvas.width - this.pixel) || this.x + this.vx < 0) {
+            this.vx = -this.vx;
+        }
+      //   this.print();
+    }
+}
+
+function Cat() {
+    Monster.call(this);
+    this.name = "cat";
+    this.text = "C";
+    this.color = "blue";
 }
 
 function Dog() {
+    Monster.call(this);
     this.name = "dog";
-    this.font = getRandomInt(14, 24) + "px serif";
     this.text = "D";
-    this.x = getRandomInt(0, 450);
-    this.y = getRandomInt(0, 450);
-    this.vx = getRandomInt(0, 5);
-    this.vy = getRandomInt(0, 2);
     this.color = "green";
-    this.show = function() {
-        ctx.font = this.font;
-        ctx.fillStyle = this.color;
-        ctx.fillText(this.text, this.x, this.y);
-    }
-    this.print = function() {
-        console.log(this.name + ": x: "+this.x +" y: "+this.y);
-    }
 }
 
 function Rabbit() {
+    Monster.call(this);
     this.name = "rabbit";
-    this.font = getRandomInt(18, 36) + "px serif";
     this.text = "R";
-    this.x = getRandomInt(10, 450);
-    this.y = getRandomInt(10, 450);
-    this.vx = getRandomInt(0, 5);
-    this.vy = getRandomInt(0, 2);
     this.color = "red";
-    this.show = function() {
-        ctx.font = this.font;
-        ctx.fillStyle = this.color;
-        ctx.fillText(this.text, this.x, this.y);
-    }
-    this.print = function() {
-        console.log(this.name + ": x: "+this.x +" y: "+this.y);
-    }
 }
 
 
@@ -77,18 +73,7 @@ var animals = [cat1, dog1, cat2, cat3, rab1, dog2, rab2];
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     for (var i = 0; i < animals.length; i++) {
-      (function() {
-          this.show();
-          this.x += this.vx;
-          this.y += this.vy;
-          if (this.y + this.vy > canvas.height || this.y + this.vy < 0) {
-              this.vy = -this.vy;
-          }
-          if (this.x + this.vx > canvas.width || this.x + this.vx < 0) {
-              this.vx = -this.vx;
-          }
-        //   this.print();
-      }).call(animals[i], i);
+      animals[i].draw.call(animals[i]);
     }
     raf = window.requestAnimationFrame(draw);
 }
